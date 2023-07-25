a5187ab6c35af2b21165d83138cd07b8854e9d68
Paweł Szopiński <szopinskipawel@gmail.com>
2023-07-25 22:50:00 +0200

    Dependencies and Structure
    -added react toolkit,
    -added base structure,
    -started coding Slice

---
+++ src/redux/phonebookSlice.jsx
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const phonebookSlice = createSlice({
+  name: 'phonebook',
+  initialState: {
+    contacts: [],
+    filter: '',
+  },
+  reducers: {
+    addContact: (state, action) => {
+      state.contacts.push(action.payload);
+    },
+    removeContact: (state, action) => {},
+  },
+});
