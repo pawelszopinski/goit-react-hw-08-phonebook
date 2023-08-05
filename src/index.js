8bd43d7b89a549b2a3471e76c981e389f1db7728
Paweł Szopiński <szopinskipawel@gmail.com>
2023-08-05 15:43:46 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
+      <Router basename='goit-react-hw-08-phonebook'>
         <App />
       </Router>
     </Provider>
