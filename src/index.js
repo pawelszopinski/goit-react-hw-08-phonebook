51add8aea0e77ad4fc140eea5200251ed58c5267
Paweł Szopiński <szopinskipawel@gmail.com>
2023-08-06 12:45:10 +0200

    Added redux persist
    Changed state maintenance

--- src/index.js
+++ src/index.js
@@ -3,15 +3,20 @@ import ReactDOM from 'react-dom/client';
 import App from 'components/App';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
-import store from 'redux/store';
+import {store,persistor} from 'redux/store';
+
+
+import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <Router basename='goit-react-hw-08-phonebook'>
         <App />
       </Router>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,14 +1,32 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import contactsReducer from './phonebookSlice';
 import authReducer from './authSlice';
-import { getUserInfo } from './authSlice';
+
+const contactsPersistConfig = {
+  key: 'phonebook',
+  storage,
+  whitelist: ['contacts'], // Wprowadź nazwę reducera, którego stan chcesz zachować w localStorage
+};
+
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['isLoggedIn', 'token', 'user'], // Wprowadź nazwy pól stanu reducera auth, które chcesz zachować w localStorage
+};
+
+const persistedContactsReducer = persistReducer(contactsPersistConfig, contactsReducer);
+const persistedAuthReducer = persistReducer(authPersistConfig, authReducer);
 
 const store = configureStore({
   reducer: {
-    phonebook: contactsReducer,
-    auth: authReducer,
-    devTools: true,
+    phonebook: persistedContactsReducer,
+    auth: persistedAuthReducer,
   },
 });
-store.dispatch(getUserInfo());
-export default store;
+
+const persistor = persistStore(store);
+
+export { store, persistor };
